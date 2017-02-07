cd01220379e7669022ed31a8a0c217ce7944b160
sergey.lysyh <sergey.lysyh@bigdropinc.com>
2017-02-07 12:14:46 +0200

    fixed masonry

--- assets/js/modules/GLOBAL.js
+++ assets/js/modules/GLOBAL.js
@@ -596,7 +596,7 @@ export default {
             html += '</div></article></div>';
           }
           html = $(html);
-          $(window).on('load', function (e) {
+          $('img').on('load', function (e) {
             $('.masonry').append(html).isotope('appended', html).isotope('layout');
             setTimeout(function () {
               if ($('.masonry .masonry-item:not(:has(form)):visible').length >= parseInt($('.tab-item.active[data-all]').attr('data-all'))) {
